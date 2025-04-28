52449b71d0aaeca7e1d55dc2a11bc3837fd86c37
Akshita2020 <akshita.tiwari2020@gmail.com>
2025-04-28 18:24:39 +0530

    Actions in redux-toolkit

--- screens/Home/Home.jsx
+++ screens/Home/Home.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
+import {SafeAreaView, Pressable, Text} from 'react-native';
 import globalStyle from '../../assets/styles/globalStyle';
 import Header from '../../components/Header/Header';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import {updateFirstName} from '../../redux/reducers/Users';
 
 const Home = () => {
   const user = useSelector(state => state.user);
+  const dispatch = useDispatch();
   console.log(user);
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
       <Header title={user.firstName + ' ' + user.lastName} />
+      <Pressable onPress={() => dispatch(updateFirstName('Akshu'))}>
+        {' '}
+        <Text> Press me to change firstName </Text>
+      </Pressable>
     </SafeAreaView>
   );
 };
